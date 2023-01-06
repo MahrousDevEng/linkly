2541a2b2d68e49f3a368437c82f82185e97f2dec
Mohamed Mahrous <eng.mahrous17@gmail.com>
2023-01-06 22:13:42 +0200

    upload product page

--- js/inner-product.js
+++ js/inner-product.js
@@ -49,63 +49,88 @@ document.addEventListener("DOMContentLoaded", () => {
     });
   }
 
-  // Trigger Products Slider
-  const commomProps = {
-    slidesPerView: 1.25,
-    speed: 700,
-    spaceBetween: 12,
-    navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
-    },
-    lazy: {
-      enabled: true,
-      loadPrevNext: true,
-    },
-    breakpoints: {
-      576: {
-        slidesPerView: 2.25,
+  // Product Big Images Slider
+
+  if (
+    document.getElementById("productBigImage") &&
+    document.getElementById("productThumbImage")
+  ) {
+    const thumbSwiper = new Swiper("#productThumbImage", {
+      loop: true,
+      spaceBetween: 10,
+      slidesPerView: 4,
+      freeMode: true,
+      watchSlidesProgress: true,
+      speed: 700,
+      lazy: {
+        enabled: true,
+        loadPrevNext: true,
       },
-      768: {
-        slidesPerView: 2.5,
-        spaceBetween: 20,
+    });
+
+    new Swiper("#productBigImage", {
+      loop: true,
+      spaceBetween: 10,
+      zoom: true,
+      speed: 700,
+      thumbs: {
+        swiper: thumbSwiper,
       },
-      992: {
-        slidesPerView: 3.25,
-        spaceBetween: 20,
+      lazy: {
+        enabled: true,
+        loadPrevNext: true,
       },
-      1200: {
-        slidesPerView: 3.5,
-        spaceBetween: 20,
+      autoplay: {
+        pauseOnMouseEnter: true,
+        reverseDirection: true,
       },
-      1400: {
-        slidesPerView: 4.25,
-        spaceBetween: 20,
+      navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
       },
-    },
-  };
+    });
+  }
 
   // Best Seller
-  if (document.querySelector(".product_slider_1")) {
-    new Swiper(".product_slider_1", {
+  if (document.getElementById("relatedProducts")) {
+    new Swiper("#relatedProducts", {
+      slidesPerView: 1.25,
+      speed: 700,
+      spaceBetween: 12,
+      lazy: {
+        enabled: true,
+        loadPrevNext: true,
+      },
       autoplay: {
         delay: 3000,
         pauseOnMouseEnter: true,
         reverseDirection: true,
       },
-      ...commomProps,
-    });
-  }
-
-  // Offers
-  if (document.querySelector(".product_slider_2")) {
-    new Swiper(".product_slider_2", {
-      autoplay: {
-        delay: 4000,
-        pauseOnMouseEnter: true,
-        reverseDirection: true,
+      navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
       },
-      ...commomProps,
+      breakpoints: {
+        576: {
+          slidesPerView: 2.25,
+        },
+        768: {
+          slidesPerView: 2.5,
+          spaceBetween: 20,
+        },
+        992: {
+          slidesPerView: 3.25,
+          spaceBetween: 20,
+        },
+        1200: {
+          slidesPerView: 3.5,
+          spaceBetween: 20,
+        },
+        1400: {
+          slidesPerView: 4.25,
+          spaceBetween: 20,
+        },
+      },
     });
   }
 });
