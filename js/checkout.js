5a1e5fbf1b1dc917cc110c15addcefcc7c7950be
Mohamed Mahrous <eng.mahrous17@gmail.com>
2023-01-14 21:46:06 +0200

    upload checkout

---
+++ js/checkout.js
@@ -0,0 +1,25 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const wz_class = ".wizard";
+
+  let args = {
+    wz_class: wz_class,
+    wz_nav_style: "progress",
+    wz_button_style: ".btn",
+    wz_ori: "horizontal",
+    buttons: true,
+    prev: "السابق",
+    next: "التالى",
+    finish: "اتمام الطلب",
+  };
+
+  const wizard = new Wizard(args);
+
+  wizard.init();
+
+  const $wz_doc = document.querySelector(wz_class);
+
+  // Submit
+  $wz_doc.addEventListener("submitWizard", function (e) {
+    location.href = "thank-you.html";
+  });
+});
